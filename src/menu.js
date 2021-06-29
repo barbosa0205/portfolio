5e5d767ad85f86d0a7683efd8aa3a75ec3caff1e
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-06-29 17:08:53 -0500

    home view finished

---
+++ src/menu.js
@@ -0,0 +1,16 @@
+const sidebar = document.querySelector('.sidebar');
+const btnMenu = document.querySelector('#btn-menu');
+
+let menuCondition = false;
+
+const toggleMenu = (condition) => {
+	if (condition === false) {
+		sidebar.className += ' sidebar-open';
+		return (menuCondition = true);
+	} else {
+		sidebar.classList.remove('sidebar-open');
+		return (menuCondition = false);
+	}
+};
+
+btnMenu.addEventListener('click', () => toggleMenu(menuCondition));
