70fc9e60bfdcfeac6a9e6f46c76222ae8ddc0ece
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-08-12 02:01:47 -0500

    Fixing wave in dark mode

--- src/changeColor.js
+++ src/changeColor.js
@@ -1,4 +1,6 @@
 const $btnMoon = document.querySelector('#btn-moon');
+const $wave = document.querySelector('.wave');
+let dark = false;
 
 $btnMoon.addEventListener('click', () => {
     document.body.classList.toggle('dark');
